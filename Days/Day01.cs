8a79c716feb394d108f2bf88a8b5888b300cfc3d
Andy <onsubmit@outlook.com>
2022-12-01 12:47:33 -0800

    Add day 1 part 1

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -16,7 +16,37 @@ namespace AdventOfCode2022.Days
         /// <returns>The solution for this day.</returns>
         public string GetSolution()
         {
-            return "0";
+            using StreamReader sr = new("input\\Day01.txt");
+
+            int maxCalories = int.MinValue;
+            int runningSum = 0;
+
+            while (true)
+            {
+                string? line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    // Next elf
+                    maxCalories = Math.Max(maxCalories, runningSum);
+                    runningSum = 0;
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    continue;
+                }
+
+                if (!int.TryParse(line, out int calories))
+                {
+                    throw new InvalidDataException();
+                }
+
+                runningSum += calories;
+            }
+
+            return maxCalories.ToString();
         }
     }
 }
