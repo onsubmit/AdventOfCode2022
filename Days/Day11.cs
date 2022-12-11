dd7c610835e4dac90b28875db09b042f64a687cb
Andy <onsubmit@outlook.com>
2022-12-11 14:17:48 -0800

    Add day 11 part 1

---
+++ Days/Day11.cs
@@ -0,0 +1,183 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Day11.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AdventOfCode2022.Days
+{
+    using System.Text.RegularExpressions;
+    using AdventOfCode2022.Models;
+
+    /// <summary>
+    /// Calculates the solution for the particular day.
+    /// </summary>
+    internal class Day11 : IDay
+    {
+        private const int MaxMonkeyBusinessRounds = 20;
+
+        private static readonly Regex MonkeyRegex = new(@"^Monkey \d+:$", RegexOptions.Compiled);
+        private static readonly Regex StartItemsRegex = new(@"^  Starting items: (?<ITEMS>(\d+(, )?)+)$", RegexOptions.Compiled);
+        private static readonly Regex OperationRegex = new(@"^  Operation: new = old (?<OPERATOR>[*+]) (?<VALUE>(old|\d+))$", RegexOptions.Compiled);
+        private static readonly Regex TestRegex = new(@"^  Test: divisible by (?<VALUE>\d+)$", RegexOptions.Compiled);
+        private static readonly Regex TestResultRegex = new(@"^    If (?<CONDITION>(true|false)): throw to monkey (?<MONKEY>\d+)$", RegexOptions.Compiled);
+        private readonly Dictionary<Regex, Action<Match, Monkey>> actionMap;
+
+        private readonly List<Monkey> monkeys = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day11"/> class.
+        /// </summary>
+        public Day11()
+        {
+            this.actionMap = new()
+            {
+                {
+                    // Starting items: 79, 98
+                    StartItemsRegex,
+                    this.SetStartingItems
+                },
+                {
+                    // Operation: new = old * 19
+                    OperationRegex,
+                    this.SetOperation
+                },
+                {
+                    // Test: divisible by 23
+                    TestRegex,
+                    this.SetTest
+                },
+                {
+                    // If true: throw to monkey 2
+                    TestResultRegex,
+                    this.SetTestResult
+                },
+            };
+        }
+
+        /// <summary>
+        /// Gets the solution for this day.
+        /// </summary>
+        /// <returns>The solution for this day.</returns>
+        public string GetSolution()
+        {
+            using StreamReader sr = new("input\\Day11.txt");
+
+            Monkey? monkey = null;
+
+            string? line;
+            while (true)
+            {
+                line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        // Blank line indicates end of monkey definition.
+                        this.monkeys.Add(monkey ?? throw new InvalidDataException());
+                    }
+
+                    if (line == null)
+                    {
+                        // EOF
+                        break;
+                    }
+
+                    continue;
+                }
+
+                if (MonkeyRegex.IsMatch(line))
+                {
+                    // Monkey 0:
+                    monkey = new();
+                    continue;
+                }
+
+                if (monkey == null)
+                {
+                    throw new InvalidDataException();
+                }
+
+                bool lineParsed = false;
+                foreach (var kvp in this.actionMap)
+                {
+                    Match match = kvp.Key.Match(line);
+                    if (match.Success)
+                    {
+                        kvp.Value(match, monkey);
+                        lineParsed = true;
+                        break;
+                    }
+                }
+
+                if (!lineParsed)
+                {
+                    throw new InvalidDataException();
+                }
+            }
+
+            return this.GetMonkeyBusinessLevel().ToString();
+        }
+
+        private void SetStartingItems(Match match, Monkey monkey)
+        {
+            monkey.Items = new(match.Groups["ITEMS"].Value.Split(',').Select(n => int.Parse(n.Trim())));
+        }
+
+        private void SetOperation(Match match, Monkey monkey)
+        {
+            string oper = match.Groups["OPERATOR"].Value;
+            string valueString = match.Groups["VALUE"].Value;
+
+            monkey.Operation = (old) =>
+            {
+                int value = valueString == "old" ? old : int.Parse(valueString);
+                return oper == "+" ? old + value : old * value;
+            };
+        }
+
+        private void SetTest(Match match, Monkey monkey)
+        {
+            int value = int.Parse(match.Groups["VALUE"].Value);
+            monkey.Test = (worryLevel) => worryLevel % value == 0;
+        }
+
+        private void SetTestResult(Match match, Monkey monkey)
+        {
+            string condition = match.Groups["CONDITION"].Value;
+            int monkeyIndex = int.Parse(match.Groups["MONKEY"].Value);
+
+            if (condition == "true")
+            {
+                monkey.TestTrueMonkey = monkeyIndex;
+            }
+            else
+            {
+                monkey.TestFalseMonkey = monkeyIndex;
+            }
+        }
+
+        private int GetMonkeyBusinessLevel()
+        {
+            Dictionary<int, int> activityLevel = Enumerable.Range(0, this.monkeys.Count).ToDictionary(x => x, x => 0);
+
+            for (int i = 0; i < MaxMonkeyBusinessRounds; i++)
+            {
+                for (int m = 0; m < this.monkeys.Count; m++)
+                {
+                    Monkey monkey = this.monkeys[m];
+                    activityLevel[m] += monkey.Items.Count;
+
+                    while (monkey.Items.TryDequeue(out int worryLevel))
+                    {
+                        int newWorryLevel = monkey.Operation(worryLevel) / 3;
+                        int newMonkey = monkey.Test(newWorryLevel) ? monkey.TestTrueMonkey : monkey.TestFalseMonkey;
+                        this.monkeys[newMonkey].Items.Enqueue(newWorryLevel);
+                    }
+                }
+            }
+
+            IEnumerable<int> top2 = activityLevel.Values.OrderByDescending(x => x).Take(2);
+            return top2.First() * top2.Last();
+        }
+    }
+}
---
+++ Models/Monkey.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Monkey.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AdventOfCode2022.Models
+{
+    /// <summary>
+    ///  Represents a monkey.
+    /// </summary>
+    internal class Monkey
+    {
+        /// <summary>
+        /// Gets or sets the queue of items inspected by the monkey.
+        /// </summary>
+        public Queue<int> Items { get; set; } = new();
+
+        /// <summary>
+        /// Gets or sets a function that describes how your worry level changes as that monkey inspects an item.
+        /// </summary>
+        public Func<int, int> Operation { get; set; } = (worryLevel) => 0;
+
+        /// <summary>
+        /// Gets or sets a function that describes how the monkey uses your worry level to decide where to throw an item next.
+        /// </summary>
+        public Func<int, bool> Test { get; set; } = (worryLevel) => false;
+
+        /// <summary>
+        /// Gets or sets the index of the destination monkey when <see cref="Test"/> is <c>true</c>.
+        /// </summary>
+        public int TestTrueMonkey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the index of the destination monkey when <see cref="Test"/> is <c>false</c>.
+        /// </summary>
+        public int TestFalseMonkey { get; set; }
+    }
+}
