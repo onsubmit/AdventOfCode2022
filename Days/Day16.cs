41c4b792b073a51520f76a64cb8463f1b45ef938
Andy <onsubmit@outlook.com>
2022-12-17 13:30:31 -0800

    Add day 16 placeholder

---
+++ Days/Day16.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Day16.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AdventOfCode2022.Days
+{
+    /// <summary>
+    /// Calculates the solution for the particular day.
+    /// </summary>
+    [Skip("Not implemented")]
+    internal class Day16 : IDay
+    {
+        /// <summary>
+        /// Gets the solution for this day.
+        /// </summary>
+        /// <returns>The solution for this day.</returns>
+        public string GetSolution()
+        {
+            return 0.ToString();
+        }
+    }
+}
